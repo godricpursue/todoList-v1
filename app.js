de7144c76fa87a67c33019724e0561ca5be455f5
Altan Simsir <altansimsir@outlook.com>
2022-08-28 14:57:44 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy cards", "Talk with friends", "Organize an event"];
+let workItems = [];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+
+
+  res.render("list", {
+    listTitle: day,
+    addedItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work"){
+    workItems.push(item);
+
+    res.redirect("/work");
+  } else {
+    items.push(item);
+
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work to Do",
+    addedItems: workItems
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
